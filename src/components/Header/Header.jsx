d3ef4562236411db31b6ebda124326af4249b5a9
Roman Lunev <Roman.Lunev@noveogroup.com>
2020-02-14 20:15:48 +0300

    Fix styles for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,15 @@ import MenuRow from './../Menu/MenuRow/MenuRow';
 import logo from './../../images/logo/logo-songbird.svg';
 import './Header.css';
 
-function Header({score}) {
+function Header({
+  score,
+  step
+}) {
   return (
     <header>
       <img src={logo} alt="logo" height="35" />
       <span id="score"> Score: {score}</span>
-      <MenuRow />
+      <MenuRow step={step}/>
     </header>
   );
 }
--- src/components/Menu/MenuItem/MenuItem.jsx
+++ src/components/Menu/MenuItem/MenuItem.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './MenuItem.css';
 
-function MenuItem({name}) {
-  return (
-    <li className="menu__item">
+function MenuItem({name, isCurrent}) {
+  if (isCurrent) {
+    return <li className="menu__item current">
         {name}
     </li>
-  );
+  } else {
+    return (
+      <li className="menu__item">
+          {name}
+      </li>
+    );
+  }
 }
 
 export default MenuItem;
